49d7dccb53294dd4a52d7ba7a0a0231327a63025
lara-ocon <202115710@alu.comillas.edu>
2023-11-27 19:59:08 +0100

    añadimos fotos datos

--- grabar_video.py
+++ grabar_video.py
@@ -3,7 +3,8 @@ from picamera2 import Picamera2
 
 def stream_video():
     picam = Picamera2()
-    picam.preview_configuration.main.size=(1280, 720)
+    picam.video_configuration.main.size=(640, 480)
+    # picam.preview_configuration.main.size=(1280, 720)
     picam.preview_configuration.main.format="RGB888"
     picam.preview_configuration.align()
     picam.configure("preview")
