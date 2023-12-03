f8e949836aaf055532a822d30b3b6a5482878040
lara-ocon <202115710@alu.comillas.edu>
2023-12-03 23:27:24 +0100

    Subo posibles cambios

--- Patrones_con_SIFT/detector.py
+++ Patrones_con_SIFT/detector.py
@@ -28,7 +28,8 @@ def mostrar_texto(frame, texto):
     return frame
  
  
-def detectar_patron_y_mostrar_texto(patron, not_patron, picam):
+# funcion de deteccion de patron
+def detectar_patron(patron, not_patron, picam):
     desbloqueado = False
     salir = False
  
@@ -58,7 +59,7 @@ def detectar_patron_y_mostrar_texto(patron, not_patron, picam):
                         elif best_match[2] < len(good):
                             best_match = ["patron", i, len(good)]
                 except:
-                    print("error en matches")
+                    ...
  
             for i in range(5, 11):
                 kp_np, des_np = not_patron[i]
@@ -74,35 +75,39 @@ def detectar_patron_y_mostrar_texto(patron, not_patron, picam):
                         elif best_match[2] < len(good):
                             best_match = ["not_patron", i, len(good)]
                 except:
-                    print("error en matches")
+                    ...
  
             if best_match is not None:
                 if best_match[0] == "patron":
                     if best_match[1] == next_pattern:
-                        print(f'es el patron que toca con {best_match[2]}')
+                        print(f'Es el patron que toca con {best_match[2]}')
                         current_pattern = next_pattern
                         next_pattern += 1
                         if next_pattern == 5:
-                            print("desbloqueado")
+                            # DESBLOQUEADO
                             desbloqueado = True
                             salir = True
+                            frame = mostrar_texto(frame, "DESBLOQUEADO")
+                        else:
+                            frame = mostrar_texto(frame, "Patron")
                     elif best_match[1] == current_pattern:
-                        print('seguimos en el patron que toca')
+                        # Seguimos en el mismo patron
+                        ...
+                        # print('Seguimos en el patron que toca.')
                     else:
-                        print('el patron es de la secuencia pero no toca')
-                        #current_pattern = 0
+                        print('Patron detectado correctamente pero no es el que toca.')
                         desbloqueado = False
                         salir = True
+
                 elif best_match[0] == "not_patron":
-                    print('es un num que no es de la secuencia')
-                    frame = mostrar_texto(frame, "No patrón")
+                    print('Patron detectado pero no es de la secuencia.')
+                    frame = mostrar_texto(frame, "No patron")
                     salir = True
                 else:
-                    print('no se ha encontrado nada')
-                    frame = mostrar_texto(frame, "No se ha encontrado nada")
+                    print('No se ha detectado ningun patron')
+                    # frame = mostrar_texto(frame, "No se ha encontrado nada")
                     pass
  
-        frame = mostrar_texto(frame, "Patrón")
         cv2.imshow("picam", frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
@@ -123,7 +128,7 @@ if __name__ == "__main__":
     desbloqueado = False
     while not desbloqueado:
         frame = picam.capture_array()
-        desbloqueado = detectar_patron_y_mostrar_texto(patron, not_patron, picam)
+        desbloqueado = detectar_patron(patron, not_patron, picam)
  
     print('Desbloqueado')
     picam.stop()
